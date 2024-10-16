d0602c5fc6547e0d521a5694816080fe8f7ac5ee
Shikhar Varshney <shikharvarshney10@gmail.com>
2024-10-16 23:15:27 +0530

    Fix SocitIO Build

--- frontend/dist/assets/customTooltip-BEAf0yRD.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-B4JSXv2W.js";import{T as t,a as e,b as l,c as a}from"./tooltip-Bhe-c_IJ.js";const d=({content:r,trigger:i,isTextOnly:s=!0})=>o.jsx(t,{children:o.jsxs(e,{delayDuration:200,children:[o.jsx(l,{asChild:!0,children:i}),o.jsx(a,{children:s?o.jsx("p",{children:r}):r})]})});export{d as C};
--- frontend/dist/assets/index-CHtsU63p.js
+++
@@ -1,1 +0,0 @@
-import"./index-B4JSXv2W.js";function o(){throw Error("an error occurs while loading this component")}export{o as default};
